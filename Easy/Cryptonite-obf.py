0824bef8c5ddbc86ca5a9256762bd8280c73765a
Algorithms of KLEIN <92029487+exotic-inx@users.noreply.github.com>
2024-08-21 23:41:04 +0600

    Add files via upload

---
+++ Easy/Cryptonite-obf.py
@@ -0,0 +1,102 @@
+#!/data/data/com.termux/files/usr/bin/python3.11
+# -*- coding: utf-8 -*-
+
+globals()['mol'] = (bool) if bool(bool(bool(bool))) < bool(type(int(141)>int(1513)<int(1714)>int(712))) and bool(str(str(517)>int(156)<int(79)>int(213))) > 2 else bool
+globals()['co2'] =  (str) if bool(bool(bool(str))) < bool(type(int(1310)>int(819)<int(16)>int(23))) and bool(str(str(813)>int(89)<int(61)>int(134))) > 2 else str
+globals()['feo2'] =  (type) if bool(bool(bool(type))) < bool(type(int(510)>int(617)<int(28)>int(1311))) and bool(str(str(613)>int(714)<int(115)>int(315))) > 2 else type
+globals()['h2so4'] =  (int) if bool(bool(bool(int))) < bool(type(int(1715)>int(89)<int(39)>int(1817))) and bool(str(str(171)>int(174)<int(15)>int(165))) > 2 else int
+globals()['feso4'] =  (bytes) if bool(bool(bool(bytes))) < bool(type(int(117)>int(34)<int(178)>int(135))) and bool(str(str(1517)>int(47)<int(1710)>int(313))) > 2 else bytes
+globals()['agno3'] =  (vars) if bool(bool(bool(vars))) < bool(type(int(165)>int(814)<int(1514)>int(518))) and bool(str(str(1516)>int(1415)<int(165)>int(162))) > 2 else vars
+globals()['h2'] =  (callable) if bool(bool(bool(callable))) < bool(type(int(91)>int(1613)<int(815)>int(1810))) and bool(str(str(12)>int(98)<int(142)>int(141))) > 2 else callable
+globals()['ch2oh4p2so4'] =  (__import__) if bool(bool(bool(__import__))) < bool(type(int(1219)>int(116)<int(119)>int(109))) and bool(str(str(16)>int(417)<int(72)>int(26))) > 2 else __import__
+globals()['tryᅠ'] =  (print) if bool(bool(bool(print))) < bool(type(int(1912)>int(115)<int(316)>int(118))) and bool(str(str(175)>int(56)<int(1214)>int(133))) > 2 else print
+globals()['exceptᅠ'] =  (input) if bool(bool(bool(input))) < bool(type(int(818)>int(78)<int(169)>int(612))) and bool(str(str(124)>int(51)<int(810)>int(113))) > 2 else input
+
+def h2o(july,*k):
+    if k:
+        klein19 = '+'
+        op = "+"
+    else:
+        klein19 = ''
+        op = ''
+    globals()['_78'] = (lambda: (lambda _315: _315 + (lambda : H2SbF7(30584))())(0) == 1)()
+    globals()['h2o'] = h2o
+    globals()['co2'] = co2
+    globals()['july'] = july
+    for globals()['klein19_'] in globals()['july']:
+        if not _78:globals()['klein19_'] += (lambda : '')()
+        klein19 += co2(klein19_);f = (lambda: (lambda _104: _104 + (lambda : H2SbF7(30584))())(0) == 1)()
+    return klein19
+
+def H2SbF7(x):
+    return h2so4(x-0x7777)
+
+def c2h6(e):
+    br = bytearray(e[len(b"klein19/"):])
+    r = 0
+    for b in br:
+        r = r * 256 + b
+    return r
+
+if (lambda: (lambda _144: _144 + (lambda : H2SbF7(30584))())(0) == 1)():
+    def o2(h2so3):
+        h2so3 = h2so3-0xFF78FF
+        if h2so3 <= 0x7F:
+                    return co2(feso4([h2so3]),"utf8")
+        elif h2so3 <= 0x7FF:
+                    if 1<2:
+                            b1 = 0xC0 | (h2so3 >> 6)
+                    b2 = 0x80 | (h2so3 & 0x3F)
+                    return co2(feso4([b1, b2]),"utf8")
+        elif h2so3 <= 0xFFFF:
+                b1 = 0xE0 | (h2so3 >> 12)
+                if 2>1:
+                    b2 = 0x80 | ((h2so3 >> 6) & 0x3F)
+                b3 = 0x80 | (h2so3 & 0x3F)
+                return co2(feso4([b1, b2, b3]),"utf8")
+        else:
+            b1 = 0xF0 | (h2so3 >> 18)
+            if 2==2:
+                b2 = 0x80 | ((h2so3 >> 12) & 0x3F)
+            if 1<2<3:
+                b3 = 0x80 | ((h2so3 >> 6) & 0x3F)
+            b4 = 0x80 | (h2so3 & 0x3F)
+            return co2(feso4([b1, b2, b3, b4]),"utf8")
+
+    def _hex(j):
+        h2so3 = ''
+        for _hex in j:
+            h2so3 += (o2(_hex))
+        return h2so3
+else:"klein19"
+
+def bytecode():
+    walid = globals().update
+    if True:
+        walid({**{ _hex([16742756, 16742765]): _1110 for _101, _1110 in agno3(ch2oh4p2so4(_hex([16742764, 16742752, 16742769, 16742770, 16742759, 16742752, 16742763]))).items() if h2(_1110) and _101 == _hex([16742763, 16742766, 16742752, 16742755, 16742770])}, **{_101: _1110 for _101, _1110 in agno3(ch2oh4p2so4(_hex([16742764, 16742752, 16742769, 16742770, 16742759, 16742752, 16742763]))).items() if h2(_1110) and _101 != _hex([16742763, 16742766, 16742752, 16742755, 16742770])}})
+    else:"walid"
+    if 1>2:
+        (lambda: c2h6(b'klein19/\x03'))()
+    else:
+        walid({**{_hex([16742761, 16742772, 16742763, 16742776]): _1110 for _101, _1110 in agno3(ch2oh4p2so4(_hex([16742777, 16742763, 16742760, 16742753]))).items() if h2(_1110) and _101 == _hex([16742755, 16742756, 16742754, 16742766, 16742764, 16742767, 16742769, 16742756, 16742770, 16742770])}, **{_101: _1110 for _101, _1110 in agno3(ch2oh4p2so4(_hex([16742777, 16742763, 16742760, 16742753]))).items() if h2(_1110) and _101 != _hex([16742755, 16742756, 16742754, 16742766, 16742764, 16742767, 16742769, 16742756, 16742770, 16742770])}})
+    walid({**{_hex([16742753, 16742760, 16742769, 16742771, 16742759]): _1110 for _101, _1110 in agno3(ch2oh4p2so4(_hex([16742753, 16742777, 16742705]))).items() if h2(_1110) and _101 == _hex([16742755, 16742756, 16742754, 16742766, 16742764, 16742767, 16742769, 16742756, 16742770, 16742770])}, **{_101: _1110 for _101, _1110 in agno3(ch2oh4p2so4(_hex([16742753, 16742777, 16742705]))).items() if h2(_1110) and _101 != _hex([16742755, 16742756, 16742754, 16742766, 16742764, 16742767, 16742769, 16742756, 16742770, 16742770])}})
+    walid()
+    walid({**{_hex([16742750, 16742704, 16742712]): _914 for _101, _914 in agno3(ch2oh4p2so4(_hex([16742753, 16742752, 16742770, 16742756, 16742709, 16742707]))).items() if h2(_914) and _101 == _hex([16742753, 16742711, 16742708, 16742755, 16742756, 16742754, 16742766, 16742755, 16742756])}, **{_101: _914 for _101, _914 in agno3(ch2oh4p2so4(_hex([16742753, 16742752, 16742770, 16742756, 16742709, 16742707]))).items() if h2(_914) and _101 != _hex([16742753, 16742711, 16742708, 16742755, 16742756, 16742754, 16742766, 16742755, 16742756])}})
+    walid()
+    walid({**{_hex([16742774, 16742752, 16742763, 16742760, 16742755]): _914 for _101, _914 in agno3(ch2oh4p2so4(_hex([16742753, 16742772, 16742760, 16742763, 16742771, 16742760, 16742765, 16742770]))).items() if h2(_914) and _101 == _hex([16742756, 16742775, 16742756, 16742754])}, **{_101: _914 for _101, _914 in agno3(ch2oh4p2so4(_hex([16742753, 16742772, 16742760, 16742763, 16742771, 16742760, 16742765, 16742770]))).items() if h2(_914) and _101 != _hex([16742756, 16742773, 16742752, 16742763])}})
+bytecode()
+
+_en                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                =b'LRx4!F+o`-Q(1(2<0Ak9P=Ejb|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBs25-CHeeZW~r`uKCdwqMabLp42zWYzT?mC)i^p8{2^dr<BQ_3`G(TSm>YHX&B4^tYRlS3z{^&W}pJw{ANskCYvQ$s+}<uM+UPe{{4(@-?^4K|?k%4w#Ck4R|I>THHcc`;8zQ`1vT9%z%)+L(t^(haGmWS%LZr>M<C)EZ<xCWn;t$eyFr_NZx^o}k)~Nt0?lMw$(%4Lw828f_-jdW{+bLr+oYjLK#y=+bGlPa=9xDB7MV9)xU@MvqY1CYoY04^ZkoCZ496V2v9?K$>kM0it4hnWTA4jieh>WJ5-tgfKv9nreEUi1AD{Hj`yNG{lU^Ml?;PlM^(aqa$iCjil2PG>nE#G7UDSjWqQ#G8t27nK3;z9+N?)O{8Krlh6~?+K)tINs;M2JyEBonKYQx@+Xr~vSk}dqd?k>n3`zQPbe~Jp@LwZDWe*iWYZutexQaln3`#)sM98cH5sX)08G%B13)wy6B<k=m<EK#hMH&rpqgkJU;qIa1i%0Q0%?E%69Q@KVrizB0WqZ70007MfE`V#k5Ei)6!e&$gu~PZiH#bVOqr>G6Goa~CPs&#0GOFGKpG~{k3dXJo`eQQ&;(!+gHue<0R|?935lkSGGIVB@01-%!AsKPc!R@8Dg=FVHPzg{=$Mn!wcIG`#WQrOnnhG3B4sF5`E^6|5;F`UQpUvwIanMM|1~O*RmJZsW~zxJfQn|RDmJ7dLDz+m>np^2^hSmr&L7}#2%@53&0vMIdn^TDx@6c>NySAJ!c~<ys|+fSM1~s@glOx=L>6nUKp}Z1IsF1*12n?dQIR`<CMg~M0VeJrs8mF|$sYR0NRi5rBe*1f0W~c=x-qLj4?ClqQ3hQZ?8QnYQ&~2^dLG@C1LE_oPR6xD?axU'
+_july                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                =b'<Gsuz{tMDMXqoh$CsM{0qFk-<o1<g{kJz%pUwYtid6s_I>DSUGGsg7_cl&=J`(jfu{Yq_nXU1>I%FCM~Mafw9eK)d(Ua%Fif`K73enG$?=!2l8Bv4mx)oblT~g7mV8OQSwYZM9)08494#u%+ne1lvj0ds?;hjN(RtE>w84fbFH_e5_Y^o6~eQMcrn-dCD2F8kvvr?>=xYf^?bd(5GLOd#q>m2?Qi0@3!`gdJ3*Gv?uckY1CW5Qe5j8@P|u<S+#_T)T3_0*2^wP-tw^dk||)l`t@~~z))H)LP7Bkz1`NzQ?u9<t}HrfV`{#HE|CsI6<<XS1Z(M}nA3e|!S_{zs*wqMq>BYZrUqyqwf8ChCcq8o4puc|5@|8!528N&YC^s#q{Um%?Y;nTu!9h;wWv;0{gc2=0GiA|$O(af76CnkMXV9G>YOb&7eg<eF5z>B8AlDYO41V5SiBLu_&^sJ|5GbOuFPhwQ>~7k51+suubEM)q5@%+jk9!a8Cht_N5F_OCP-HmwP4vRUqcU>l6PL`6qn57tW6evKFPCrptdso9CQ8{oB59M<TYk%0Z84qFB2x~btHC_bKS}+FqRscbK!I@MENf$EQ%{3<26ugvRUIEQ@{<I1UyX<N5=ZRt{}l?$6Zbl9HCC7gnRzB5+F-d>uFuyTSh8^m8+UG9FQ06RsD>dL!Z*wFn_w1HEkfGdA=Z(YgsIsFz%C*YC@1$MQF?##-a8ye-&Yp1VpU^CF{8^`1u8riaQC-gd^-jMCLA(BsnoF+v$|lgN<E>lO+b{Ss&ufXhk@jtyb=10yjrr?%j5{DoY+-MVt#n1Fu=W3Uen<Oyw-~Zk)eD+8Kv6_GHQc5iqb*<(F*^>|Y=*Oqr_1xo{REU4q|*)5wc!oZl3~ANmo}SPs9rxfceOS?a|Z04Av<pK_Ohlx>TvLq#WQwBJ>b@UxTufe9'
+_birth                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                =b'Y!!T)$?=SthP`xVtOWg$ooKFqktA@jgSvS$N%YVCG*alHWwGd?FDq&IN%gezt00G2`km@?eR)bqx1)US5cLuG(DVw{RZIpxT8)cla2ouAPx4Xb|)(2?>B_Vrj&#h=%AhK34#ExU<S9P{Ly@{-5V11R9^y#Iu~fHlU1(t--4?=)R~AJRKlI>^rogrvk+aUuy?IJ`fqzlgvy3oWRW!*}4w)=M8?FaR(#S_DfLcio|Uy4EPe&<|)zDV8&lTfC?Osw4wYUc2vC&~ap0Nj)_mNo$bV^@MScZC?cCzJ*Wg&@#}0xyJ@gU<LI(!k%Szm=*AvS51KkvXy|^7T`WUcSb?_1k~s|bGlLhL<!>iQ87kM0+5}{8J=Hr6vq8XBz9YiUh^RI#jq^KWTsacD|^a3-zk{_oqA8XKKm?9_eG$B9;V|7&5ti(GdyXP4A_I$Og!6xtRO-=2`lLlMidlXS&_ohdu|Y0-xJXi$9#7BwG8M#QJs!U`m`^l;7!kY7>~YoyW`I3P<aSiWU>q#K;$GsO5wf0zR<|IU?PpS0_^t-ohLNb@g}kyB8tjh#TCFdCmxH$(<4B0!`%0n1Y^exvB6K9>lrHkWkCU!&}B$l%AIWd$uy-<ik_0i*abJHE>5qumUZ#|ZMog^Egzra>~5&GFJ{Rai6$u7e>b(G2*p7l7rnX|rR1G>t)K%_r;ipKTO4({Y2OYcCP|GkB~jeuDN<s8v1m-vb_L_L$Pxtd1ap})Q^Bem-K(p}JsrkHdJVNH!y{Se!{#`5W-Dwj<|Yee*8JguL{(Xf;SsS8LDLv}>anpKf+W3`SLE!0{@fav`kDr$<At=r3eNngxjkK~v7|Fn9yEgchY{!E3ICiQ3|RAZLvD|vS`9N2jne}PJ1Ia%#_EhZK&20eqXj#o<Eug3;6g=jH1&6j&g)lHJ>fB4MYe*?)23r<4{4ZM)R'
+__19                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                =b'XmQeq^V#ne!<^Cp6=BL8wBMwK2_`mk1!OzcR|IDZX>X*^YR!Q3^FeFvhPx&ysXaW)#@UUfd$Wo;)RjHk0x|2O5`mzwy_?Qz|AgM9a;t0_f^^r#?<zdfC}+)C4+z1T<2RJI~EIk0nek>g_|ti-CM&B=7MO#qzW+GQ1H`;WuxN_3Px8m!L=(>&BG}4fe*x&Sjft4O7j-KNbS26%DcHHR2+xH6IDE!Km&Q>fi%6-9H9bUb{{Ke7kLyDu~1yXsO@>e0*APug1tqXeWa8r*e(URN^vbTO2bqvB?nRB^Aiz*)+X~(3ykFQ@v@rY~aS}eyQ)&vY0pM5^V*4Y(S%ktAwg171#;&jKVIj1jP%W=HijP^yX#ikt}ab4|WR8&Fr}1@X|hWZ!(39wRCoMX@)I>{8YArF+s=E*N!nGnA?P$L_skV8^-*8n%IT$X{C;G{5_F048V#g^)d(@OegrMV~t%J+bDTjHM<8Hi^Qf=EHpVYfLhs$+B`6gdpLiCBo=t;Fav*6y`+Rfz+5+-EZ{8-qod+>e*&TAdLbckpLtP%r%8?2VzS6p)m}V@r*jtC&rr&f$%6$(llJjNj6_yk(HG)$NEHVt8PWoKq{_K&)WH0%ffs?XED{Q3J#kY9v}ZqH<vWQ+SDa^*TqKis8!9SAwdruM3{va$pU<^B!p)u51SmPZ7DI#TlNBpQZC2M?U70YT^F){8)YMtZU*YT+nYy_$F18cq?%GffT#Tf~r|QVUucZL52CHn!{v+9b-PGgJ5CNy7Dqd#ilJ%q447ltjezZu7_atFQ^u1E1E)NZ|OF1AEh#8ggFpk!cRu1}YG62qB=t)-k7Qo_m7NU*=*UvzPE*@@%Lj{u8*^=sc4Jgd)IlC?x1AS5iDgd5>RWk{2Q15agTp2<IUB#OJO~g*$dL~|5bn(sJF}KQVAmqV2BL9oIBAh5lLOt=3fB'
+
+try:
+    walid(
+    en(
+    july(
+    birth(
+    _19(
+    _en+_july+_birth+__19)))))
+except Exception as e:
+    print(e)
